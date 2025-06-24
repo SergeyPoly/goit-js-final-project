38619ca9501efba3a42497ba458e8e1cb9c2b035
Viacheslav Dmytriiev <v.dmytriiev@sdventures.com>
2025-06-24 23:18:25 +0200

    Added pagination element

--- src/js/pages/favorites.js
+++ src/js/pages/favorites.js
@@ -1,5 +1,6 @@
 import { EXERCISES_PAGE_SIZE } from '../constants';
 import { renderExercisesList } from '../renderers/exercises-list';
+import { renderPagination } from '../renderers/pagination';
 import { getStoredExercises } from '../services/storage';
 
 let page = 0;
@@ -15,6 +16,15 @@ function renderFavoritesList() {
     "It appears that you haven't added any exercises to your favorites yet. To get started, you can add exercises that you like to your favorites for easier access in the future.",
     true
   );
+
+  const totalPages = Math.ceil(favorites.length / EXERCISES_PAGE_SIZE);
+
+  if (totalPages > 1) {
+    renderPagination(page, totalPages, newPage => {
+      page = newPage;
+      renderFavoritesList();
+    });
+  }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
