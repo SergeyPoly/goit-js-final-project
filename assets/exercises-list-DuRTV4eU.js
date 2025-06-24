7a1d618e8ef2da9094043dd006a63b83824b4bb3
SergeyPoly <73758000+SergeyPoly@users.noreply.github.com>
2025-06-24 16:49:35 +0000

    Deploying to gh-pages from @ SergeyPoly/goit-js-final-project@d8eab24e33fa6e4b70f7d23ec7f0f943119d182f 🚀

---
+++ assets/exercises-list-DuRTV4eU.js
@@ -0,0 +1,48 @@
+(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const r of document.querySelectorAll('link[rel="modulepreload"]'))n(r);new MutationObserver(r=>{for(const o of r)if(o.type==="childList")for(const d of o.addedNodes)d.tagName==="LINK"&&d.rel==="modulepreload"&&n(d)}).observe(document,{childList:!0,subtree:!0});function s(r){const o={};return r.integrity&&(o.integrity=r.integrity),r.referrerPolicy&&(o.referrerPolicy=r.referrerPolicy),r.crossOrigin==="use-credentials"?o.credentials="include":r.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function n(r){if(r.ep)return;r.ep=!0;const o=s(r);fetch(r.href,o)}})();const i="exercise-card",a=Object.freeze({START:"start",RATE:"rate",REMOVE_FAVORITE:"removeFavorite"});function u(e,t){return`
+    <div class="${i}" data-id="${e._id}">
+      <div class="exercise-card-header">
+      <p class="exercise-equipment">${e.equipment}</p>
+        <div class="exercise-card-header-middle">
+          ${t?`<button data-action="${a.REMOVE_FAVORITE}" class="card-icon-button remove-favorite-button">
+                  <svg class="remove-favorite-icon">
+                    <use href="./img/icons.svg#trash-icon"></use>
+                  </svg>
+                </button>`:`<button data-action="${a.RATE}" class="card-icon-button rating-button">
+                  ${e.rating}
+                  <svg class="rating-icon">
+                    <use href="./img/icons.svg#star-filled-icon"></use>
+                  </svg>
+                </button>`}
+        </div>
+        <button data-action="${a.START}" class="start-button">
+          Start
+          <svg class="arrow-icon" width="13" height="13">
+            <use href="./img/icons.svg#arrow-icon"></use>
+          </svg>
+        </button>
+      </div>
+      <div class="exercise-card-body">
+        <div class="exercise-name-container">
+          <svg class="exercise-icon" width="24" height="24">
+            <use href="./img/icons.svg#running-man-icon"></use>
+          </svg>
+          <h3 class="exercise-name">${e.name}</h3>
+        </div>
+      </div>
+      <div class="exercise-card-footer">
+        <p class="card-footer-text">
+          <span class="secondary-text">Burned calories:</span>
+          ${e.burnedCalories} /${e.time} min
+        </p>
+        <p class="card-footer-text">
+          <span class="secondary-text">Body part:</span>
+          ${e.bodyPart}
+        </p>
+        <p class="card-footer-text">
+          <span class="secondary-text">Target:</span>
+          ${e.target}
+        </p>
+      </div>
+    </div>
+  `}function f(e,t){const s=e.querySelector(`.${i}[data-id="${t}"]`);s&&s.remove()}const l="favoriteExercises";function g(e){const s=v().filter(n=>n._id!==e);p(s)}function p(e){localStorage.setItem(l,JSON.stringify(e))}function v(){try{const e=localStorage.getItem(l);if(e){const t=JSON.parse(e);if(Array.isArray(t))return t.filter(m)}}catch{}return[]}function m(e){return e&&typeof e=="object"&&typeof e._id=="string"&&typeof e.name=="string"}function E(e,t){g(t),f(e,t)}function h(e){console.log(`Rating exercise: ${e}`)}function y(e){console.log(`Starting exercise: ${e}`)}function b(e,t,s){switch(s){case a.REMOVE_FAVORITE:E(e,t);break;case a.START:y(t);break;case a.RATE:h(t);break;default:console.warn("Unknown action:",s)}}const c=document.querySelector(".exercises-list");function x(e,t,s){c.innerHTML="";let n="";e.length?e.forEach(r=>{n+=u(r,s)}):n=`<p class="no-exercises-message">${t}</p>`,c.insertAdjacentHTML("beforeend",n)}c.addEventListener("click",e=>{let t=e.target;if(t.nodeName!=="BUTTON"&&(t=t.closest(`.${i} button`)),!t||t.nodeName!=="BUTTON")return;const n=t.closest(`.${i}`).dataset.id,r=t.dataset.action;b(c,n,r)});export{v as g,x as r};
+//# sourceMappingURL=exercises-list-DuRTV4eU.js.map
---
+++ favorites-page.js
@@ -0,0 +1,2 @@
+import{r as e,g as t}from"./assets/exercises-list-DuRTV4eU.js";e(t(),"It appears that you haven't added any exercises to your favorites yet. To get started, you can add exercises that you like to your favorites for easier access in the future.",!0);
+//# sourceMappingURL=favorites-page.js.map
