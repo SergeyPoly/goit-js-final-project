6442981fd2adcf628e4def357f0ae08f51f50e97
Viacheslav Dmytriiev <v.dmytriiev@sdventures.com>
2025-06-25 19:58:25 +0200

    Completed pagination and removeFavorite logic

--- src/js/pages/favorites.js
+++ src/js/pages/favorites.js
@@ -3,27 +3,41 @@ import { renderExercisesList } from '../renderers/exercises-list';
 import { renderPagination } from '../renderers/pagination';
 import { getStoredExercises } from '../services/storage';
 
-let page = 0;
-const favorites = getStoredExercises();
+let page = 1;
+let favorites = getStoredExercises();
+
+function handlePageChange(newPage) {
+  page = newPage;
+  renderFavoritesList();
+}
+
+function handleFavoriteDelete(exerciseId) {
+  favorites = favorites.filter(exercise => exercise._id !== exerciseId);
+  renderFavoritesList();
+}
 
 function renderFavoritesList() {
-  const start = page * EXERCISES_PAGE_SIZE;
-  const end = (page + 1) * EXERCISES_PAGE_SIZE;
+  const totalPages = Math.ceil(favorites.length / EXERCISES_PAGE_SIZE);
+
+  if (page > totalPages) {
+    // if the page exceeds the total pages, reset to the last page
+    // if totalPages is 0, set page to 1
+    page = totalPages || 1;
+  }
+
+  const start = (page - 1) * EXERCISES_PAGE_SIZE;
+  const end = page * EXERCISES_PAGE_SIZE;
+
   const exercisesToRender = favorites.slice(start, end);
 
   renderExercisesList(
     exercisesToRender,
     "It appears that you haven't added any exercises to your favorites yet. To get started, you can add exercises that you like to your favorites for easier access in the future.",
-    true
+    { favorites: true, onFavoriteDelete: handleFavoriteDelete }
   );
 
-  const totalPages = Math.ceil(favorites.length / EXERCISES_PAGE_SIZE);
-
   if (totalPages > 1) {
-    renderPagination(page, totalPages, newPage => {
-      page = newPage;
-      renderFavoritesList();
-    });
+    renderPagination(page, totalPages, handlePageChange);
   }
 }
 
