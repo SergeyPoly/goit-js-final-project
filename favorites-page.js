b09acfdfe571a73b5a7e0691e5640a1eb3676c81
SergeyPoly <73758000+SergeyPoly@users.noreply.github.com>
2025-06-25 19:34:51 +0000

    Deploying to gh-pages from @ SergeyPoly/goit-js-final-project@cadd248aaaf136c83dc86ea092726938d64dd84c 🚀

--- favorites-page.js
+++ favorites-page.js
@@ -1,2 +1,16 @@
-import{g as u,l as p,r as f,E as i}from"./assets/header-B6eVmwhF.js";function E(a,s,r){let e=document.querySelector(".pagination");e?e.innerHTML="":(e=document.createElement("div"),e.className="pagination",document.querySelector(".favorites-content").appendChild(e));let n="";for(let t=0;t<s;t+=1)n+=`<button class="page-button ${t===a?"active":""}" data-page="${t}">${t+1}</button>`;e.insertAdjacentHTML("beforeend",n),e.onclick=t=>{const c=t.target.nodeName==="BUTTON"?t.target:t.target.closest(".pagination button");if(c){const d=parseInt(c.dataset.page,10);d!==a&&r(d)}}}let o=0;const g=u();function l(){const a=o*i,s=(o+1)*i,r=g.slice(a,s);f(r,"It appears that you haven't added any exercises to your favorites yet. To get started, you can add exercises that you like to your favorites for easier access in the future.",!0);const e=Math.ceil(g.length/i);e>1&&E(o,e,n=>{o=n,l()})}document.addEventListener("DOMContentLoaded",()=>{l(),p()});
+import{g as x,l as y,E as f,r as S}from"./assets/header-DgzlJhPB.js";const d=3;function E(){return'<span class="pagination-ellipsis">...</span>'}function L(t,e){return`<button class="page-button ${t===e?"active":""}" data-page="${t}">${t}</button>`}function c(t,e,o,a){return`<button class="page-button arrow ${a?"disabled":""}" data-page="${o}">
+    <svg class="pagination-arrow-icon ${e?"double":""}">
+      <use href="./img/icons.svg#pagination-arrow${e?"-double":""}-${t}-icon"></use>
+    </svg>
+  </button>`}function T(t,e,o){let a=document.querySelector(".pagination");a?a.innerHTML="":(a=document.createElement("div"),a.className="pagination",document.querySelector(".exercises-list").appendChild(a));let r="";const $=Math.max(1,t-Math.floor(d/2)),u=Math.min(e,$+d-1),p=Math.max(1,u-d+1),v=e>d;if(v){const n=t===1;r+=`
+      <div class="arrow-buttons">
+        ${c("left",!0,1,n)}
+        ${c("left",!1,t-1,n)}
+      </div>
+    `}let i="";for(let n=p;n<=u;n+=1)i+=L(n,t);const m=p>1,M=u<e;if(m&&(i=`${E()}${i}`),M&&(i+=E()),r+=`<div class="page-buttons">${i}</div>`,v){const n=t===e;r+=`
+      <div class="arrow-buttons">
+        ${c("right",!1,t+1,n)}
+        ${c("right",!0,e,n)}
+      </div>
+    `}a.insertAdjacentHTML("beforeend",r),a.onclick=n=>{const h=n.target.nodeName==="BUTTON"?n.target:n.target.closest(".pagination button");if(h){const b=parseInt(h.dataset.page,10);b!==t&&o(b)}}}let s=1,l=x();function A(t){s=t,g()}function w(t){l=l.filter(e=>e._id!==t),g()}function g(){const t=Math.ceil(l.length/f);s>t&&(s=t||1);const e=(s-1)*f,o=s*f,a=l.slice(e,o);S(a,"It appears that you haven't added any exercises to your favorites yet. To get started, you can add exercises that you like to your favorites for easier access in the future.",{favorites:!0,onFavoriteDelete:w}),t>1&&T(s,t,A)}document.addEventListener("DOMContentLoaded",()=>{g(),y()});
 //# sourceMappingURL=favorites-page.js.map
