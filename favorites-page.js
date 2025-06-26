a5e6319e7d406021de8be6a8e0a25dbe8650aabf
SergeyPoly <73758000+SergeyPoly@users.noreply.github.com>
2025-06-26 10:56:17 +0000

    Deploying to gh-pages from @ SergeyPoly/goit-js-final-project@f8d77240a72c94b5ccc9b8e786670a40edc5f7a7 🚀

--- favorites-page.js
+++ favorites-page.js
@@ -1,16 +1,16 @@
-import{g as x,l as y,E as f,r as S}from"./assets/header-COwEqoJH.js";const d=3;function E(){return'<span class="pagination-ellipsis">...</span>'}function L(t,e){return`<button class="page-button ${t===e?"active":""}" data-page="${t}">${t}</button>`}function c(t,e,o,a){return`<button class="page-button arrow ${a?"disabled":""}" data-page="${o}">
+import{g as x,a as y,l as S,E as f,r as L}from"./assets/header-CTCnJ5Ir.js";const d=3;function b(){return'<span class="pagination-ellipsis">...</span>'}function T(t,e){return`<button class="page-button ${t===e?"active":""}" data-page="${t}">${t}</button>`}function c(t,e,o,n){return`<button class="page-button arrow ${n?"disabled":""}" data-page="${o}">
     <svg class="pagination-arrow-icon ${e?"double":""}">
-      <use href="./img/icons.svg#pagination-arrow${e?"-double":""}-${t}-icon"></use>
+      <use href="${x(`pagination-arrow${e?"-double":""}-${t}-icon`)}"></use>
     </svg>
-  </button>`}function T(t,e,o){let a=document.querySelector(".pagination");a?a.innerHTML="":(a=document.createElement("div"),a.className="pagination",document.querySelector(".exercises-list").appendChild(a));let r="";const $=Math.max(1,t-Math.floor(d/2)),u=Math.min(e,$+d-1),p=Math.max(1,u-d+1),v=e>d;if(v){const n=t===1;r+=`
+  </button>`}function A(t,e,o){let n=document.querySelector(".pagination");n?n.innerHTML="":(n=document.createElement("div"),n.className="pagination",document.querySelector(".exercises-list").appendChild(n));let r="";const E=Math.max(1,t-Math.floor(d/2)),u=Math.min(e,E+d-1),g=Math.max(1,u-d+1),h=e>d;if(h){const a=t===1;r+=`
       <div class="arrow-buttons">
-        ${c("left",!0,1,n)}
-        ${c("left",!1,t-1,n)}
+        ${c("left",!0,1,a)}
+        ${c("left",!1,t-1,a)}
       </div>
-    `}let i="";for(let n=p;n<=u;n+=1)i+=L(n,t);const m=p>1,M=u<e;if(m&&(i=`${E()}${i}`),M&&(i+=E()),r+=`<div class="page-buttons">${i}</div>`,v){const n=t===e;r+=`
+    `}let i="";for(let a=g;a<=u;a+=1)i+=T(a,t);const m=g>1,M=u<e;if(m&&(i=`${b()}${i}`),M&&(i+=b()),r+=`<div class="page-buttons">${i}</div>`,h){const a=t===e;r+=`
       <div class="arrow-buttons">
-        ${c("right",!1,t+1,n)}
-        ${c("right",!0,e,n)}
+        ${c("right",!1,t+1,a)}
+        ${c("right",!0,e,a)}
       </div>
-    `}a.insertAdjacentHTML("beforeend",r),a.onclick=n=>{const h=n.target.nodeName==="BUTTON"?n.target:n.target.closest(".pagination button");if(h){const b=parseInt(h.dataset.page,10);b!==t&&o(b)}}}let s=1,l=x();function A(t){s=t,g()}function w(t){l=l.filter(e=>e._id!==t),g()}function g(){const t=Math.ceil(l.length/f);s>t&&(s=t||1);const e=(s-1)*f,o=s*f,a=l.slice(e,o);S(a,"It appears that you haven't added any exercises to your favorites yet. To get started, you can add exercises that you like to your favorites for easier access in the future.",{favorites:!0,onFavoriteDelete:w}),t>1&&T(s,t,A)}document.addEventListener("DOMContentLoaded",()=>{g(),y()});
+    `}n.insertAdjacentHTML("beforeend",r),n.onclick=a=>{const v=a.target.nodeName==="BUTTON"?a.target:a.target.closest(".pagination button");if(v){const $=parseInt(v.dataset.page,10);$!==t&&o($)}}}let s=1,l=y();function w(t){s=t,p()}function C(t){l=l.filter(e=>e._id!==t),p()}function p(){const t=Math.ceil(l.length/f);s>t&&(s=t||1);const e=(s-1)*f,o=s*f,n=l.slice(e,o);L(n,"It appears that you haven't added any exercises to your favorites yet. To get started, you can add exercises that you like to your favorites for easier access in the future.",{favorites:!0,onFavoriteDelete:C}),t>1&&A(s,t,w)}document.addEventListener("DOMContentLoaded",()=>{p(),S()});
 //# sourceMappingURL=favorites-page.js.map
