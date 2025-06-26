8b2eecc77498227433547d84679470d6bbf26837
AlexSimakhin <oleksandrsimakhin@gmail.com>
2025-06-26 15:51:30 +0300

    fix: fixed adding quote to exercises filters block

--- src/js/pages/home.js
+++ src/js/pages/home.js
@@ -1,8 +1,8 @@
-// import { renderQuote } from '../renderers/quote';
+import { renderQuote } from '../renderers/quote';
 import { loadHeader } from '../services/header'
 import '../renderers/section-exercises';
 
 export const homePage = () => {
   loadHeader();
-  // renderQuote();
+  renderQuote();
 }
